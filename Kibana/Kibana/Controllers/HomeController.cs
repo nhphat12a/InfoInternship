4111a26f2642929063baaa553c373fb769b70c34
nhphat12a <ngpht53@gmail.com>
2022-12-06 20:29:12 +0700

    test

---
+++ Kibana/Kibana/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Kibana.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> logger;
+
+        // GET: api/<HomeController>
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            logger.LogError("Errorr1111");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
